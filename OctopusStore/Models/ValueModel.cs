46afa78162b301e18cf448633bb12ec4e7bdee22
andy <andy@andydote.co.uk>
2015-07-13 22:12:57 +0100

    model: add model

---
+++ OctopusStore/Models/ValueModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OctopusStore.Models
+{
+	public class ValueModel
+	{
+		public int CreateIndex { get; set; }
+		public int ModifyIndex { get; set; }
+		public int LockIndex { get; set; }
+		public string Key { get; set; }
+		public int Flags { get; set; }
+		public string Value { get; set; }
+		public string Session { get; set; }
+	}
+}
