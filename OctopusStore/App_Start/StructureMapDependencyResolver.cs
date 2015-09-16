92f33f370b85ef3478387cc1920c890b14643b01
andy <andy@andydote.co.uk>
2015-09-16 22:08:55 +0100

    container: add structuremap

---
+++ OctopusStore/App_Start/StructureMapDependencyResolver.cs
@@ -0,0 +1,19 @@
+using System.Web.Http.Dependencies;
+using StructureMap;
+
+namespace OctopusStore
+{
+	public class StructureMapDependencyResolver : StructureMapDependencyScope, IDependencyResolver
+	{
+		public StructureMapDependencyResolver(IContainer container)
+			: base(container)
+		{
+		}
+
+		public IDependencyScope BeginScope()
+		{
+			var child = Container.GetNestedContainer();
+			return new StructureMapDependencyScope(child);
+		}
+	}
+}
--- OctopusStore/App_Start/WebApiConfig.cs
+++ OctopusStore/App_Start/WebApiConfig.cs
@@ -1,15 +1,18 @@
 ﻿using System.Web.Http;
 using Conifer;
 using Conifer.Conventions;
+using OctopusStore.Config;
 using OctopusStore.Consul;
+using StructureMap;
+using StructureMap.Graph;
 
 namespace OctopusStore
 {
 	public static class WebApiConfig
 	{
-		public static void Register(HttpConfiguration config)
+		public static void Register(HttpConfiguration http)
 		{
-			var router = new Router(config, e =>
+			var router = new Router(http, e =>
 			{
 				e.DefaultConventionsAre(new IRouteConvention[]
 				{
@@ -20,6 +23,19 @@ namespace OctopusStore
 
 				e.AddAll<KeyValueController>();
 			});
+
+			var container = new Container(config =>
+			{
+				config.Scan(a =>
+				{
+					a.TheCallingAssembly();
+					a.WithDefaultConventions();
+				});
+
+				config.For<IConfiguration>().Use<Configuration>().Singleton();
+			});
+
+			http.DependencyResolver = new StructureMapDependencyResolver(container);
 		}
 	}
 }
