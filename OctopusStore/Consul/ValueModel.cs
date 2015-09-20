36aa3a0e9d7efbe4282f07a813a1bfdd8e770221
andy <andy@andydote.co.uk>
2015-09-20 16:45:41 +0100

    valueModel: make createIndex and modifyIndex nullable

--- OctopusStore/Consul/ValueModel.cs
+++ OctopusStore/Consul/ValueModel.cs
@@ -2,12 +2,14 @@
 {
 	public class ValueModel
 	{
-		public int CreateIndex { get; set; }
-		public int ModifyIndex { get; set; }
+		public int? CreateIndex { get; set; }
+		public int? ModifyIndex { get; set; }
+
 		public int LockIndex { get; set; }
+		public string Session { get; set; }
+
 		public string Key { get; set; }
 		public int Flags { get; set; }
 		public string Value { get; set; }
-		public string Session { get; set; }
 	}
 }
