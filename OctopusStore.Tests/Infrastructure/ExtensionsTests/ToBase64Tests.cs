7e9498a1e7e0719729cbb9367107cc4dd44f00bb
andy <andy@andydote.co.uk>
2015-09-22 22:30:37 +0100

    tests: add .toBase64() tests

---
+++ OctopusStore.Tests/Infrastructure/ExtensionsTests/ToBase64Tests.cs
@@ -0,0 +1,15 @@
+﻿using OctopusStore.Infrastructure;
+using Shouldly;
+using Xunit;
+
+namespace OctopusStore.Tests.Infrastructure.ExtensionsTests
+{
+	public class ToBase64Tests
+	{
+		[Fact]
+		public void When_converting_a_string()
+		{
+			"test".ToBase64().ShouldBe("dGVzdA==");
+		}
+	}
+}
