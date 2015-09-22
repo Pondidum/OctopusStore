f629040d7c49438c4d0fb133b9eb54b7937997bf
andy <andy@andydote.co.uk>
2015-09-22 22:17:01 +0100

    test: add .doFirst() tests

---
+++ OctopusStore.Tests/Infrastructure/ExtensionsTests/DoFirstTests.cs
@@ -0,0 +1,43 @@
+﻿using OctopusStore.Infrastructure;
+using Shouldly;
+using Xunit;
+
+namespace OctopusStore.Tests.Infrastructure.ExtensionsTests
+{
+	public class DoFirstTests
+	{
+		[Fact]
+		public void When_running_on_an_empty_collection()
+		{
+			var collection = new Model[] { };
+
+			Should.NotThrow(() => collection.DoFirst(m => m.Value = "yes"));
+		}
+
+		[Fact]
+		public void When_running_on_a_single_item_collection()
+		{
+			var collection = new[] { new Model() };
+
+			collection.DoFirst(m => m.Value = "yes");
+
+			collection[0].Value.ShouldBe("yes");
+		}
+
+		[Fact]
+		public void When_running_on_a_multi_item_collection()
+		{
+			var collection = new[] { new Model(), new Model() };
+
+			collection.DoFirst(m => m.Value = "yes");
+
+			collection[0].Value.ShouldBe("yes");
+			collection[1].Value.ShouldBe("");
+		}
+
+		private class Model
+		{
+			public string Value = "";
+		}
+	}
+}
