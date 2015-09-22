c7da6ac692207a5e5aca7d175a206cb74224cbfb
andy <andy@andydote.co.uk>
2015-09-22 22:16:40 +0100

    test: add equalsIgnore() tests

---
+++ OctopusStore.Tests/Infrastructure/ExtensionsTests/EqualsIgnoreTests.cs
@@ -0,0 +1,27 @@
+﻿using OctopusStore.Infrastructure;
+using Shouldly;
+using Xunit;
+
+namespace OctopusStore.Tests.Infrastructure.ExtensionsTests
+{
+	public class EqualsIgnoreTests
+	{
+		[Fact]
+		public void When_the_strings_match_exactly()
+		{
+			"test".EqualsIgnore("test").ShouldBe(true);
+		}
+
+		[Fact]
+		public void When_the_string_differ_by_case()
+		{
+			"test".EqualsIgnore("tESt").ShouldBe(true);
+		}
+
+		[Fact]
+		public void WHen_the_strings_are_different()
+		{
+			"test".EqualsIgnore("no.").ShouldBe(false);
+		}
+	}
+}
